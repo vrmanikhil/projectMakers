53e3a7b18d89697c83b30a3dba5079ef8e371764
Nikhil Verma <vrmanikhil@gmail.com>
2017-01-29 19:45:28 +0530

    menu and newsletter subscribitions

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -31,6 +31,8 @@ class Home extends CI_Controller {
 		$home['home'] = $this->data_lib->getHomeContent();
 		$home['home'] = $home['home'][0];
 		$home['images'] = $this->data_lib->getImages();
+		$home['csrf_token_name'] = $this->security->get_csrf_token_name();
+		$home['csrf_token'] = $this->security->get_csrf_hash();
 		$home['testimonials'] = $this->data_lib->getTestimonials();
 		$vm['body'] = $this->load->view('pages/home', $home, TRUE);
 		$vm['headerContent'] = $this->data_lib->getHeaderContent();
@@ -57,6 +59,8 @@ class Home extends CI_Controller {
 		$vm['activePage'] = 'about';
 		$about['images'] = $this->data_lib->getImages();
 		$about['about'] = $this->data_lib->getAboutContent();
+		$about['csrf_token_name'] = $this->security->get_csrf_token_name();
+		$about['csrf_token'] = $this->security->get_csrf_hash();
 		$vm['body'] = $this->load->view('pages/about', $about, TRUE);
 		$vm['headerContent'] = $this->data_lib->getHeaderContent();
 		$vm['headerContent'] = $vm['headerContent'][0];
@@ -84,6 +88,8 @@ class Home extends CI_Controller {
 		$team['teamContent'] = $this->data_lib->getTeamContent();
 		$team['teamContent'] = $team['teamContent'][0]['content'];
 		$team['team'] = $this->data_lib->getTeam();
+		$team['csrf_token_name'] = $this->security->get_csrf_token_name();
+		$team['csrf_token'] = $this->security->get_csrf_hash();
 		$vm['body'] = $this->load->view('pages/team', $team, TRUE);
 		$vm['headerContent'] = $this->data_lib->getHeaderContent();
 		$vm['headerContent'] = $vm['headerContent'][0];
@@ -181,8 +187,57 @@ public function menu()
 	];
 	$vm['title'] = 'Menu For Order | Makers';
 	$vm['activePage'] = 'menu';
-	$vm['body'] = $this->load->view('pages/menu', '', TRUE);
+	$menu['csrf_token_name'] = $this->security->get_csrf_token_name();
+	$menu['csrf_token'] = $this->security->get_csrf_hash();
+	$menu['menuCategories'] = $this->data_lib->getMenuCategories();
+	$menu['menuItems'] = $this->data_lib->getMenuItems();
+	$menu['products'] = array();
+	foreach ($menu['menuCategories'] as $key => $value) {
+		$menu['products'][$value['name']] = array('name' => $value['name'], 'description'=> $value['description'], 'items' => array());
+	}
+	foreach ($menu['menuItems'] as $key => $value) {
+		array_push($menu['products'][$value['category']]['items'], $value);
+	}
+	$menu['mobileNumber'] = $this->data_lib->getMenuMobile();
+	$menu['mobileNumber'] = $menu['mobileNumber'][0]['contact'];
+	$vm['body'] = $this->load->view('pages/menu', $menu, TRUE);
+	$vm['headerContent'] = $this->data_lib->getHeaderContent();
+	$vm['headerContent'] = $vm['headerContent'][0];
+	$vm['footerContent'] = $this->data_lib->getFooterContent();
+	$vm['footerContent'] = $vm['footerContent'][0];
 	$this->load->view('template', $vm);
+}
+
+public function subscribeNewsletter(){
+	$email = '';
+	if($x = $this->input->post('email')){
+		$email = $x;
+	}
+	if($email == ''){
+		$this->session->set_flashdata('message', array('content'=>'Incomplete Data','color'=>'red'));
+		redirect(base_url());
+	}
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+		$this->session->set_flashdata('message', array('content'=>'There is some error with your E-Mail Address Format','color'=>'red'));
+		redirect(base_url());
+	}
+	$data = array(
+		'email' => $email
+	);
+	$check = $this->data_lib->emailExist($email);
+	if(!empty($check)){
+		$this->session->set_flashdata('message', array('content'=>'You are already subscribed to our newsletters','color'=>'red'));
+		redirect(base_url());
+	}
+	$result = $this->data_lib->subscribeNewsletter($data);
+	if($result){
+		$this->session->set_flashdata('message', array('content'=>'Newsletter Subscribtion Successful','color'=>'green'));
+		redirect(base_url());
+	}
+	else{
+		$this->session->set_flashdata('message', array('content'=>'Something Went Wrong','color'=>'red'));
+		redirect(base_url());
+	}
 }
 
 
--- application/views/pages/menu.php
+++ application/views/pages/menu.php
@@ -6,66 +6,45 @@
 <section>
 	<div class="container-fluid fixed-width-container">
 		<nav class="products-menu flex">
-			<a class="products-menu__item js-products-menu__item active " href="#cookies">Cookies</a>
-			<a class="products-menu__item js-products-menu__item" href="#cakes">Cakes</a>
-			<a class="products-menu__item js-products-menu__item" href="javascript:">Breads</a>
-			<a class="products-menu__item js-products-menu__item" href="javascript:">Savoury Treats</a>
-			<a class="products-menu__item js-products-menu__item" href="javascript:">Chocolates</a>
+			<?php foreach ($products as $key => $value) {
+				$href = strtolower($key);
+				$href = preg_replace('/\s+/','-', $href);
+				 ?>
+				<a class="products-menu__item js-products-menu__item" href="<?php echo "#".$href; ?>"><?php echo $key; ?></a>
+			<?php } ?>
+
+
 		</nav>
-		<div class="products-category" id="cookies">
-			<h2 class="products-category__title text-center">Cookies</h2>
-			<p class="products-category__desc text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+		<?php foreach ($products as $key => $value) {
+			$href = strtolower($key);
+			$href = preg_replace('/\s+/','-', $href);
+			?>
+		<div class="products-category" id="<?php echo $href; ?>">
+			<h2 class="products-category__title text-center"><?php echo $key; ?></h2>
+			<div class="products-category__desc text-center"><?php echo $value['description']; ?></div>
 			<div class="products flex">
+				<?php foreach ($value['items'] as $key2 => $value2) {  ?>
+
 				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Chocochip cookies</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
+					<img class="product__image product__info" src="<?php echo $value2['imageURL']; ?>">
+					<p class="product__name product__info"><?php echo $value2['itemName']; ?></p>
+					<div class="product__desc product__info"><?php echo $value2['itemDescription']; ?></div>
+					<p class="product__price product__info">Starts from Rs. <?php echo $value2['startsFrom']; ?></p>
+					<p class="product__contact-info">Call  +91 <?php echo $mobileNumber; ?> to know more</p>
 				</div>
-				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Chocochip cookies</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
-				</div>
-				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Chocochip cookies</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet</p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
-				</div>
+
+				<?php	} ?>
+
 			</div>
 		</div>
-		<div class="products-category" id="cakes">
-			<img src="/web-assets/image/design.png" class="products-category__divider">
-			<h2 class="products-category__title text-center">Cakes</h2>
-			<p class="products-category__desc text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-			<div class="products flex">
-				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Tea Cakes</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
-				</div>
-				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Tea Cakes</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
-				</div>
-				<div class="product flex flex-col">
-					<img class="product__image product__info" src="/web-assets/image/product.png">
-					<p class="product__name product__info">Tea Cakes</p>
-					<p class="product__desc product__info">Lorem ipsum dolor sit amet</p>
-					<p class="product__price product__info">Starts from Rs. 150</p>
-					<p class="product__contact-info">Call  +91 9876543210 to know more</p>
-				</div>
-			</div>
-		</div>
+
+
+		<?php } ?>
+
+
+
+
+
 	</div>
 </section>
